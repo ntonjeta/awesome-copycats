04e7564b36f2c59b843b448aa430d47ff46f3958
ntonjeta <ntonjeta@autistici.org>
2021-10-25 08:55:02 +0200

    fixed broken screenshot shoprtcut conflict with firefox tab pinner

--- rc.lua
+++ rc.lua
@@ -260,7 +260,7 @@ root.buttons(my_table.join(
 globalkeys = my_table.join(
     -- Take a screenshot
     -- https://github.com/lcpz/dots/blob/master/bin/screenshot
-    awful.key({ altkey }, "p", function() os.execute("screenshot") end,
+    awful.key({ altkey, "Control" }, "p", function() os.execute("screenshot") end,
               {description = "take a screenshot", group = "hotkeys"}),
 
     -- X screen locker
