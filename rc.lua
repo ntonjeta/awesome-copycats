01f0837d0a701ab87ce04a91c6922469bd45113c
ntonjeta <ntonjeta@autistici.org>
2024-10-28 09:27:13 +0100

    change minor stuff

--- rc.lua
+++ rc.lua
@@ -64,20 +64,22 @@ end
 -- This function will run once every time Awesome is started
 local function run_once(cmd_arr)
     for _, cmd in ipairs(cmd_arr) do
-        awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
+        --awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
+        -- This function implements the XDG autostart specification
+        awful.spawn.with_shell(
+            'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
+            'xrdb -merge <<< "awesome.started:true";' ..
+            -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
+            'dex --environment Awesome --autostart --search-paths "$XDG_CONFIG_DIRS/autostart:$XDG_CONFIG_HOME/autostart"' -- https://github.com/jceb/dex
+        )
     end
 end
 
 run_once({ "urxvtd", "unclutter -root" }) -- comma-separated entries
 
--- This function implements the XDG autostart specification
-awful.spawn.with_shell(
-    'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
-    'xrdb -merge <<< "awesome.started:true";' ..
-    -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
-    'dex --environment Awesome --autostart --search-paths ' ..
-    '"${XDG_CONFIG_HOME:-$HOME/.config}/autostart:${XDG_CONFIG_DIRS:-/etc/xdg}/autostart";' -- https://github.com/jceb/dex
-)
+--[[
+--]]
+
 -- }}}
 
 -- {{{ Variable definitions
