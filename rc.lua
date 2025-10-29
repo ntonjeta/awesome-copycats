8a011695f6bfc145b70fd11acf0640d5f6fa91ba
ntonjeta <ntonjeta@autistici.org>
2025-10-29 07:45:33 +0100

    add new tile tipe

--- rc.lua
+++ rc.lua
@@ -115,9 +115,9 @@ awful.layout.layouts = {
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
-    --awful.layout.suit.fair,
+    awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
-    --awful.layout.suit.spiral,
+    awful.layout.suit.spiral,
     --awful.layout.suit.spiral.dwindle,
     --awful.layout.suit.max,
     --awful.layout.suit.max.fullscreen,
@@ -130,7 +130,7 @@ awful.layout.layouts = {
     --lain.layout.cascade.tile,
     -- lain.layout.centerwork,
     lain.layout.centerwork.horizontal,
-    -- lain.layout.termfair,
+    lain.layout.termfair,
     -- lain.layout.termfair.center
 }
 
