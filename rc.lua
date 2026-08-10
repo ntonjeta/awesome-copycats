ee30b765ef523402831aefa3175206c6bbe54e63
ntonjeta <ntonjeta@autistici.org>
2026-08-10 11:06:18 +0200

    commenter no empty move

--- rc.lua
+++ rc.lua
@@ -333,13 +333,13 @@ globalkeys = mytable.join(
 	awful.key({ modkey }, "Right", awful.tag.viewnext, { description = "view next", group = "tag" }),
 	awful.key({ modkey }, "Escape", awful.tag.history.restore, { description = "go back", group = "tag" }),
 
-	-- Non-empty tag browsing
-	awful.key({ altkey }, "Left", function()
-		lain.util.tag_view_nonempty(-1)
-	end, { description = "view  previous nonempty", group = "tag" }),
-	awful.key({ altkey }, "Right", function()
-		lain.util.tag_view_nonempty(1)
-	end, { description = "view  previous nonempty", group = "tag" }),
+	---- Non-empty tag browsing
+	--awful.key({ altkey }, "Left", function()
+		--lain.util.tag_view_nonempty(-1)
+	--end, { description = "view  previous nonempty", group = "tag" }),
+	--awful.key({ altkey }, "Right", function()
+		--lain.util.tag_view_nonempty(1)
+	--end, { description = "view  previous nonempty", group = "tag" }),
 
 	-- Default client focus
 	awful.key({ altkey }, "j", function()
